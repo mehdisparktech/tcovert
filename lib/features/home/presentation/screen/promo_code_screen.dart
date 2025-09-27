0497bf17e654b8bae633930b0b628ba950d3a14b
mehdisparktech <mehdisparktech@gmail.com>
2025-09-27 16:56:51 +0600

    feat(home): add empty promo code screen

---
+++ lib/features/home/presentation/screen/promo_code_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PromoCodeScreen extends StatelessWidget {
+  const PromoCodeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
